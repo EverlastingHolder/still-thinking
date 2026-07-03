e6075b141ac785227f894d964ae18b70b4e7b238
roman.moshkovcev <roman.moshkovcev@myworkflow.ru>
2026-07-03 15:09:10 +0700

    chore: add project

---
+++ StillThinking/StillThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StillThinkingApp.swift
+//  StillThinking
+//
+//  Created by roman.moshkovcev on 03.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct StillThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("test")
+        }
+    }
+}
