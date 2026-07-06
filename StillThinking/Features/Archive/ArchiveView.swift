7bcf54999e8b0c193b21d762ed347123762268e8
roman.moshkovcev <roman.moshkovcev@myworkflow.ru>
2026-07-06 17:17:30 +0700

    feat: автообновление архива

--- StillThinking/Features/Archive/ArchiveView.swift
+++ StillThinking/Features/Archive/ArchiveView.swift
@@ -30,17 +30,16 @@ struct ArchiveView: View {
         }
         .navigationTitle("Архив")
         .searchable(text: $model.searchText, prompt: "Поиск")
-        .onChange(of: model.searchText) {
-            Task {
-                await model.load()
-            }
+        .task(id: model.searchText) {
+            await model.load()
         }
-        .onChange(of: model.selectedFilter) {
-            Task {
-                await model.load()
-            }
+        .task(id: model.selectedFilter) {
+            await model.load()
         }
-        .task {
+        .onAppear {
+            model.currentTime = .now
+        }
+        .task(id: model.currentTime) {
             if model.items.isEmpty && model.searchText.isEmpty {
                 await model.load()
             }
